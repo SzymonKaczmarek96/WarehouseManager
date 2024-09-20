c7d828d47e82b97bc2fd987fcc1e2d0bef88bd72
Szymon Kaczmarek <kaczmarekszymon96@gmail.com>
2024-09-20 21:08:55 +0200

    added user protection regarding product api

--- src/main/java/Warehouse/WarehouseManager/product/ProductController.java
+++ src/main/java/Warehouse/WarehouseManager/product/ProductController.java
@@ -2,6 +2,7 @@ package Warehouse.WarehouseManager.product;
 
 import Warehouse.WarehouseManager.enums.ProductSize;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,33 +21,37 @@ public class ProductController {
 
 
     @GetMapping
-    public ResponseEntity<List<ProductDto>> getProductList(){
-        return ResponseEntity.ok(productService.getDtoProductList());
+    public ResponseEntity<List<ProductDto>> getProductList(@RequestHeader(HttpHeaders.AUTHORIZATION) String bearerToken){
+        return ResponseEntity.ok(productService.getDtoProductList(bearerToken));
     }
 
     @GetMapping("/{name}")
-    public ResponseEntity<ProductDto> getProductByProductName(@PathVariable String name){
-        return ResponseEntity.ok(productService.getProductDtoByProductName(name));
+    public ResponseEntity<ProductDto> getProductByProductName(@PathVariable String name
+            ,@RequestHeader(HttpHeaders.AUTHORIZATION) String bearerToken){
+        return ResponseEntity.ok(productService.getProductDtoByProductName(name,bearerToken));
     }
 
     @GetMapping("/size")
-    public ResponseEntity<List<ProductDto>> getProductListByProductSize(@RequestBody ProductSize size){
-        return ResponseEntity.ok().body(productService.getProductDtoListBySize(size));
+    public ResponseEntity<List<ProductDto>> getProductListByProductSize(@RequestBody ProductSize size
+            ,@RequestHeader(HttpHeaders.AUTHORIZATION) String bearerToken){
+        return ResponseEntity.ok().body(productService.getProductDtoListBySize(size,bearerToken));
     }
 
     @PostMapping("/add")
-    public ResponseEntity<ProductDto> addProduct(@RequestBody ProductDto productDto){
-        return ResponseEntity.ok().body(productService.addProductToDataBase(productDto));
+    public ResponseEntity<ProductDto> addProduct(@RequestBody ProductDto productDto
+            ,@RequestHeader(HttpHeaders.AUTHORIZATION) String bearerToken){
+        return ResponseEntity.ok().body(productService.addProductDtoToDataBase(productDto,bearerToken));
     }
 
     @PutMapping("/{name}/update")
-    public ResponseEntity<ProductDto> updateProductInformation(@PathVariable String name, @RequestBody ProductDto productDto){
-        return ResponseEntity.ok(productService.modifyProductInformation(name,productDto));
+    public ResponseEntity<ProductDto> updateProductInformation(@PathVariable String name, @RequestBody ProductDto productDto
+    ,@RequestHeader(HttpHeaders.AUTHORIZATION) String bearerString){
+        return ResponseEntity.ok(productService.modifyProductInformation(name,productDto,bearerString));
     }
 
     @DeleteMapping("/{name}/delete")
-    public ResponseEntity deleteProduct(@PathVariable String name){
-        productService.deleteProduct(name);
+    public ResponseEntity deleteProduct(@PathVariable String name,@RequestHeader(HttpHeaders.AUTHORIZATION) String bearerToken){
+        productService.deleteProduct(name,bearerToken);
         return ResponseEntity.noContent().build();
     }
 
--- src/test/java/Warehouse/WarehouseManager/ProductServiceTest.java
+++ src/test/java/Warehouse/WarehouseManager/ProductServiceTest.java
@@ -1,15 +1,18 @@
 package Warehouse.WarehouseManager;
 
 
+import Warehouse.WarehouseManager.employee.EmployeeDto;
+import Warehouse.WarehouseManager.employee.EmployeeRepository;
 import Warehouse.WarehouseManager.enums.ProductSize;
-import Warehouse.WarehouseManager.exception.ProductAlreadyExistsException;
-import Warehouse.WarehouseManager.exception.ProductNotExistsException;
-import Warehouse.WarehouseManager.exception.ProductQuantityException;
-import Warehouse.WarehouseManager.exception.StockNotExistsException;
+import Warehouse.WarehouseManager.enums.Resource;
+import Warehouse.WarehouseManager.enums.Role;
+import Warehouse.WarehouseManager.enums.WarehouseSystemOperation;
+import Warehouse.WarehouseManager.exception.*;
 import Warehouse.WarehouseManager.product.Product;
 import Warehouse.WarehouseManager.product.ProductDto;
 import Warehouse.WarehouseManager.product.ProductRepository;
 import Warehouse.WarehouseManager.product.ProductService;
+import Warehouse.WarehouseManager.security.SecurityService;
 import Warehouse.WarehouseManager.stock.Stock;
 import Warehouse.WarehouseManager.stock.StockRepository;
 import org.junit.jupiter.api.Test;
@@ -27,34 +30,56 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
-     //algorithm
     @Mock
     private ProductRepository productRepository;
 
     @Mock
     private StockRepository stockRepository;
 
+    @Mock
+    private SecurityService securityService;
+    @Mock
+    private Role role;
+
+    @Mock
+    private EmployeeDto employeeDto;
+
     @InjectMocks
     private ProductService productService;
 
     @Test
-    public void shouldReturnProductList(){
+    public void shouldReturnProductDtoList(){
         //given
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.STORE,Resource.PRODUCT)).thenReturn(true);
         when(productRepository.findAll()).thenReturn(createProductsForTest());
         //when
-        List<ProductDto> productDtoList = productService.getDtoProductList();
+        List<ProductDto> productDtoList = productService.getDtoProductList("token");
         //then
         assertEquals(6,productDtoList.size());
     }
 
     @Test
-    public void shouldFindProductByName(){
+    public void shouldThrowAccessDeniedExceptionByReturnProductListWhenEmployeeHasNoAccess(){
         //given
-        Product product = new Product(1L,"Product1", ProductSize.MEDIUM);
-        String name = "Product1";
-        when(productRepository.findByName(name)).thenReturn(Optional.of(product));
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.STORE,Resource.PRODUCT)).thenReturn(false);
         //when
-        ProductDto productDto = productService.getProductDtoByProductName(name);
+        assertThrows(AccessDeniedException.class,()-> productService.getDtoProductList("token"));
+    }
+
+    @Test
+    public void shouldReturnProductDtoWhenProductExistsAndEmployeeHasAccess(){
+        //given
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.STORE,Resource.PRODUCT)).thenReturn(true);
+        Product product = new Product(1L,"Product1", ProductSize.MEDIUM);
+        when(productRepository.findByName("Product1")).thenReturn(Optional.of(product));
+        //when
+        ProductDto productDto = productService.getProductDtoByProductName("Product1","token");
         //then
         assertNotNull(productDto);
         assertEquals(1L,productDto.id());
@@ -63,18 +88,58 @@ public class ProductServiceTest {
     }
 
     @Test
-    public void shouldThrowProductNotExistsExceptionWhenProductNotExists(){
-        assertThrows(ProductNotExistsException.class,() -> productService.getProductDtoByProductName("Product1"));
+    public void shouldThrowAccessDeniedExceptionWhenEmployeeHasNoAccess() {
+        //given
+        when(employeeDto.role()).thenReturn(role);
+        when(role.hasAccessTo(WarehouseSystemOperation.STORE, Resource.PRODUCT)).thenReturn(false);
+        when(securityService.getEmployeeDtoFromBearerToken("validToken")).thenReturn(employeeDto);
+        //when
+        assertThrows(AccessDeniedException.class, () -> productService.getProductDtoByProductName("Product1", "validToken"));
     }
 
     @Test
-    public void shouldFindProductListByProductSize(){
+    public void shouldThrowProductNotExistsExceptionWhenProductNoExists() {
         //given
+        when(employeeDto.role()).thenReturn(role);
+        when(role.hasAccessTo(WarehouseSystemOperation.STORE, Resource.PRODUCT)).thenReturn(true);
+        when(securityService.getEmployeeDtoFromBearerToken("validToken")).thenReturn(employeeDto);
+        //when
+        assertThrows(ProductNotExistsException.class, () -> productService.getProductDtoByProductName("Product1", "validToken"));
+    }
+
+    @Test
+    public void shouldThrowProductAlreadyExistsExceptionWhenProductExists(){
+        //given
+        when(employeeDto.role()).thenReturn(role);
+        when(role.hasAccessTo(WarehouseSystemOperation.ADD, Resource.PRODUCT)).thenReturn(true);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        Product product1 = new Product(2L,"Product1", ProductSize.MEDIUM);
+        when(productRepository.existsByName("Product1")).thenReturn(true);
+        //when then
+        assertThrows(ProductAlreadyExistsException.class, () -> productService.addProductDtoToDataBase(product1.toProductDto(),"token"));
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentsExceptionWhenProductSizeIsNull(){
+        //given
+        when(employeeDto.role()).thenReturn(role);
+        when(role.hasAccessTo(WarehouseSystemOperation.ADD, Resource.PRODUCT)).thenReturn(true);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        Product product = new Product();
+        //when then
+        assertThrows(IllegalArgumentException.class,() -> productService.addProductDtoToDataBase(new Product().toProductDto(),"token"));
+    }
+    @Test
+    public void shouldFindProductListByProductSizeAndEmployeeHasAccess(){
+        //given
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.STORE,Resource.PRODUCT)).thenReturn(true);
         List<Product> mediumSize = createProductsForTest().stream()
                 .filter(product -> product.getSize().equals(ProductSize.MEDIUM)).toList();
         when(productRepository.findBySize(ProductSize.MEDIUM)).thenReturn(mediumSize);
         //when
-        List<ProductDto> productDtoList = productService.getProductDtoListBySize(ProductSize.MEDIUM);
+        List<ProductDto> productDtoList = productService.getProductDtoListBySize(ProductSize.MEDIUM,"token");
         //then
         assertEquals(4,productDtoList.size());
         assertEquals(ProductSize.MEDIUM,productDtoList.get(0).size());
@@ -84,31 +149,27 @@ public class ProductServiceTest {
     }
 
     @Test
-    public void shouldThrowProductAlreadyExistsExceptionWhenProductExists(){
+    public void shouldThrowAccessDeniedExceptionByFindProductListWhenEmployeeHasNoAccess(){
         //given
-        Product product = new Product(1L,"Product1", ProductSize.MEDIUM);
-        Product product1 = new Product(2L,"Product1", ProductSize.MEDIUM);
-        when(productRepository.existsByName(product.getName())).thenReturn(true);
-        //when then
-        assertThrows(ProductAlreadyExistsException.class, () -> productService.addProductToDataBase(product1.toProductDto()));
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.STORE,Resource.PRODUCT)).thenReturn(false);
+        //when
+        assertThrows(AccessDeniedException.class,() -> productService.getProductDtoListBySize(ProductSize.MEDIUM,"token"));
 
     }
 
-    @Test
-    public void shouldThrowIllegalArgumentsExceptionWhenProductSizeIsNull(){
-        //given
-        Product product = new Product();
-        //when then
-        assertThrows(IllegalArgumentException.class,() -> productService.addProductToDataBase(product.toProductDto()));
-    }
 
     @Test
     public void shouldAddProductToDataBase(){
         //given
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.ADD,Resource.PRODUCT)).thenReturn(true);
         Product product = new Product(1L,"Product1", ProductSize.MEDIUM);
         when(productRepository.save(any(Product.class))).thenReturn(product);
         //when
-        ProductDto productDto = productService.addProductToDataBase(product.toProductDto());
+        ProductDto productDto = productService.addProductDtoToDataBase(product.toProductDto(),"token");
         //then
         assertNotNull(productDto);
         assertEquals("Product1",productDto.name());
@@ -117,15 +178,18 @@ public class ProductServiceTest {
     }
 
     @Test
-    public void shouldModifyProductName(){
+    public void shouldModifyProductNameWhenEmployeeHasAccess(){
         //given
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.MODIFY,Resource.PRODUCT)).thenReturn(true);
         Product product = new Product(1L,"Product1", ProductSize.MEDIUM);
         Product modifyProduct = new Product(1L,"Product2",ProductSize.MEDIUM);
         ProductDto productDto = new ProductDto(1L,"Product2",ProductSize.MEDIUM);
         when(productRepository.findByName(product.getName())).thenReturn(Optional.of(product));
         when(productRepository.save(any(Product.class))).thenReturn(modifyProduct);
         //when
-        ProductDto productModified = productService.modifyProductInformation(product.getName(),productDto);
+        ProductDto productModified = productService.modifyProductInformation(product.getName(),productDto,"token");
         //then
         assertNotNull(productModified);
         assertEquals("Product2",productModified.name());
@@ -136,23 +200,41 @@ public class ProductServiceTest {
     @Test
     public void shouldModifyThrowProductNotExistsExceptionWhenProductNotExists(){
         //given
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.MODIFY,Resource.PRODUCT)).thenReturn(true);
         ProductDto productDto = new ProductDto(1L,"Product2",ProductSize.MEDIUM);
         //when then
         assertThrows(ProductNotExistsException.class,
-                () -> productService.modifyProductInformation("Product1",productDto));
+                () -> productService.modifyProductInformation("Product1",productDto,"token"));
+    }
+
+
+    @Test
+    public void shouldThrowAccessDeniedExceptionByModifyProductWhenEmployeeHasNoAccess(){
+        //given
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.MODIFY,Resource.PRODUCT)).thenReturn(false);
+        //when then
+        assertThrows(AccessDeniedException.class,() -> productService.modifyProductInformation("Product1",
+                new Product().toProductDto(),"token"));
     }
 
 
     @Test
     public void shouldDeleteProduct(){
         //given
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.REMOVAL,Resource.PRODUCT)).thenReturn(true);
         Product product = new Product(1L,"Product2",ProductSize.MEDIUM);
         Stock stock = new Stock();
         stock.setQuantity(0L);
         when(productRepository.findByName(product.getName())).thenReturn(Optional.of(product));
         when(stockRepository.findStockByProductName(product.getName())).thenReturn(Optional.of(stock));
         //when
-        productService.deleteProduct(product.getName());
+        productService.deleteProduct(product.getName(),"token");
         //then
         verify(stockRepository, times(1)).delete(stock);
         verify(productRepository, times(1)).delete(product);
@@ -160,32 +242,55 @@ public class ProductServiceTest {
 
     @Test
     public void deleteProductShouldThrowProductNotExistsExceptionWhenProductNotFound(){
+        //given
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.REMOVAL,Resource.PRODUCT)).thenReturn(true);
         Product product = new Product();
-        assertThrows(ProductNotExistsException.class, () -> productService.deleteProduct(product.getName()));
+        //when
+        assertThrows(ProductNotExistsException.class, () -> productService.deleteProduct(product.getName(),"token"));
     }
 
     @Test
     public void deleteProductShouldThrowProductQuantityExceptionWhenQuantityIsMoreThanZero(){
         //given
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.REMOVAL,Resource.PRODUCT)).thenReturn(true);
         Product product = new Product(1L,"Product2",ProductSize.MEDIUM);
         Stock stock = new Stock();
         stock.setQuantity(5L);
         when(productRepository.findByName(product.getName())).thenReturn(Optional.of(product));
         when(stockRepository.findStockByProductName(product.getName())).thenReturn(Optional.of(stock));
         //when
-        assertThrows(ProductQuantityException.class, () -> productService.deleteProduct(product.getName()));
+        assertThrows(ProductQuantityException.class, () -> productService.deleteProduct(product.getName(),"token"));
     }
 
     @Test
     public void deleteProductShouldThrowStockNotExistsExceptionWhenStockNotFound(){
+        //given
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.REMOVAL,Resource.PRODUCT)).thenReturn(true);
         Product product = new Product(1L,"Product2",ProductSize.MEDIUM);
         when(productRepository.findByName(product.getName())).thenReturn(Optional.of(product));
         Stock stock = new Stock();
         stock.setQuantity(0L);
         when(stockRepository.findStockByProductName(product.getName())).thenReturn(Optional.empty());
-        assertThrows(StockNotExistsException.class, () -> productService.deleteProduct(product.getName()));
-
+        //when
+        assertThrows(StockNotExistsException.class, () -> productService.deleteProduct(product.getName(),"token"));
     }
+
+    @Test
+    public void shouldThrowAccessDeniedExceptionIntoDeleteProductWhenEmployeeHasNoAccess(){
+        //given
+        when(employeeDto.role()).thenReturn(role);
+        when(securityService.getEmployeeDtoFromBearerToken("token")).thenReturn(employeeDto);
+        when(role.hasAccessTo(WarehouseSystemOperation.REMOVAL,Resource.PRODUCT)).thenReturn(false);
+        //when
+        assertThrows(AccessDeniedException.class,() -> productService.deleteProduct("Product","token"));
+    }
+
 
     private List<Product> createProductsForTest(){
         Product product = new Product(1L,"Product1", ProductSize.MEDIUM);
